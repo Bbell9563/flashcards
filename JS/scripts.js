c297a4155d4517bfa0912b583ee139f7d20d0f0f
Brianna Bell <bbell9563@gmail.com>
2020-03-06 09:39:30 -0700

    final commit

--- JS/scripts.js
+++ JS/scripts.js
@@ -19,43 +19,42 @@ var card1 = new Card("What is 1+1", "2")
 var card2 = new Card("How to tie your shoes", "you just don't")
 var cardsArray = [card1, card2]
 
-
 function editAll(){
   editBtn.style.display = "none"
   addBtn.style.display = "block"
   doneBtn.style.display = "block"
   exitBtn.style.display = "block"
   cardShow.innerHTML = ""
-  cardsArray.forEach((element, index) => {
+  cardsArray.forEach((e, i) => {
     cardShow.innerHTML += 
       `<div>
         <label>Question:</label>
-        <input class= "inputField" type="text" name="question" id = "question${index}" value="${element.question}">
+        <input class= "inputField" type="text"id = "question${i}" value="${e.question}">
         <label>Answer:</label>
-        <input class= "inputField" type="text" name="answer" id = "answer${index}" value="${element.answer}">
-        <h1 class = "button" id= "delete${index}" onclick = "deleteCard(${index})">Delete This Question</h1>
+        <input class= "inputField" type="text" id = "answer${i}" value="${e.answer}">
+        <h1 class = "button" id= "delete${i}" onclick = "deleteCard(${i})">Delete This Question</h1>
       </div>`
   })
 }
 
-function deleteCard (i){
+function deleteCard(i){
   cardsArray.splice(i,1)
   editAll()
 }
 
-function flipCard(index){
-  var card = document.getElementById(`q${index}`)
-  if(card.innerHTML == cardsArray[index].question){
-    card.innerHTML = cardsArray[index].answer
+function flipCard(i){
+  var card = document.getElementById(`q${i}`)
+  if(card.innerHTML == cardsArray[i].question){
+    card.innerHTML = cardsArray[i].answer
   }
-  else{card.innerHTML = cardsArray[index].question;}
+  else{card.innerHTML = cardsArray[i].question;}
   }
 
 function updateAll(){
-  cardsArray.forEach((element, index)=>{
-    let question = document.getElementById(`question${index}`)
-    let answer = document.getElementById(`answer${index}`)
-    cardsArray[index] = {question: question.value, answer: answer.value}
+  cardsArray.forEach((e, i)=>{
+    let q = document.getElementById(`question${i}`)
+    let a = document.getElementById(`answer${i}`)
+    cardsArray[i] = {question: q.value, answer: a.value}
   })
 }
 
@@ -63,15 +62,15 @@ function exitEdit(){
   doneBtn.style.display = "none"
   addBtn.style.display = "none"
   editBtn.style.display = "block"
-  showCards(cardsArray)
+  showCards()
 }
 
-function showCards(arr){
+function showCards(){
   exitBtn.style.display = "none"
   cardShow.innerHTML = ""
-  arr.forEach((element, index) => {
-    cardShow.innerHTML += `<div  id = "card" onclick = "flipCard(${index})">
-                              <h1  id = "q${index}">${cardsArray[index].question}</h1>
+  cardsArray.forEach((e, i) => {
+    cardShow.innerHTML += `<div  id = "card" onclick = "flipCard(${i})">
+                              <h1  id = "q${i}">${cardsArray[i].question}</h1>
                             </div>`
   })
 }
@@ -83,5 +82,4 @@ function createNewCard(){
   cardsArray.push(newCard)
   editAll()
 }
-showCards(cardsArray)
-
+showCards()
