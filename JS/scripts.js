2fa582e380c4718c92fee857456e607cba14ceba
Brianna Bell <bbell9563@gmail.com>
2020-03-05 20:31:14 -0700

    create and update methods working

--- JS/scripts.js
+++ JS/scripts.js
@@ -5,55 +5,79 @@ class Card {
   }
 }
 
+var editBtn = document.getElementById("editButton")
+var doneBtn = document.getElementById("doneButton")
+var newFrm = document.getElementById("newForm")
+var cardShow = document.getElementById("cardHolder")
+var exitBtn = document.getElementById("exitButton")
+var addBtn = document.getElementById("addButton")
+var cardFlip = document.getElementById("card")
+
 var card1 = new Card("What is 1+1", "2")
 var card2 = new Card("How to tie your shoes", "you just don't")
 var cardsArray = [card1, card2]
 showCards(cardsArray)
 
+editBtn.addEventListener("click", editAll)
+doneBtn.addEventListener("click", updateAll)
+exitBtn.addEventListener("click", exitEdit)
+addBtn.addEventListener("click", createNewCard)
+// cardFlip.addEventListener("click", flipCard)
 
-
-function showCards(arr){
-  var cardShow = document.getElementById("cardHolder")
+function editAll(){
+  editBtn.style.display = "none"
+  addBtn.style.display = "block"
+  doneBtn.style.display = "block"
+  exitBtn.style.display = "block"
   cardShow.innerHTML = ""
-  arr.forEach((element, index) => {
-    cardShow.innerHTML += `<div>
-                              <h1 id = q${index}>Question: ${element.question}</h1>
-                              <h1 id = a${index}> Answer:${element.answer}</h1>
-                            </div>`
-    cardShow.innerHTML += `<button id ="card${index}"onclick = "editCard(${index})">Edit This Card </button>`
+  cardsArray.forEach((element, index) => {
+    cardShow.innerHTML += 
+      `<div>
+        <label>Question:</label>
+        <input class= "inputField" type="text" name="question" id = "question${index}" value="${element.question}">
+        <label>Answer:</label>
+        <input class= "inputField" type="text" name="answer" id = "answer${index}" value="${element.answer}">
+      </div>`
   })
 }
 
-function editCard(index){
-  let card = cardsArray[index]
-  let question = document.getElementById(`q${index}`)
-  let answer = document.getElementById(`a${index}`)
-  let button = document.getElementById(`card${index}`)
-  question.innerHTML = `<input type="text" name="question" id = "question${index}" value="${card.question}">`
-  answer.innerHTML = `<input type="text" name="answer" id = "answer${index}" value="${card.answer}">`
-  button.innerHTML = `<button id = "c${index}" onclick = "updateCard(${index})">Update</button>`
+function flipCard()
 
+function updateAll(){
+  cardsArray.forEach((element, index)=>{
+    let question = document.getElementById(`question${index}`)
+    let answer = document.getElementById(`answer${index}`)
+    cardsArray[index] = {question: question.value, answer: answer.value}
+  })
 }
 
-function closeEdit(){
-  console.log("got here")
-  showCards(cardArray)
-  console.log(cardsArray)
-}
-
-function updateCard(index){
-  let question = document.getElementById(`question${index}`)
-  let answer = document.getElementById(`answer${index}`)
-  return cardsArray[index] = {question: question.value, answer: answer.value}
-}
-
-
-const createNewCard = () => {
-  let form = document.getElementById("frm");
-  let q = form[0].value
-  let a = form [1].value
-  newCard = new Card(q,a)
-  cardsArray.push(newCard)
+function exitEdit(){
+  doneBtn.style.display = "none"
+  addBtn.style.display = "none"
+  editBtn.style.display = "block"
   showCards(cardsArray)
 }
+
+function showCards(arr){
+  exitBtn.style.display = "none"
+  cardShow.innerHTML = ""
+  arr.forEach((element, index) => {
+    cardShow.innerHTML += `<div  id = "card">
+                              <h1 id = q${index}> ${element.question}</h1>
+                            </div>`
+  })
+}
+
+                              // <h1 id = a${index}> Answer:${element.answer}</h1>
+
+
+
+function createNewCard(){
+  let q = ""
+  let a = ""
+  newCard = new Card(q,a)
+  cardsArray.push(newCard)
+  editAll()
+}
+showCards(cardsArray)
 
